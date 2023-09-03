97ec025cfbc060eda2141d12431811052af108c6
Alex Mur <lehamuraschov2014@yandex.ru>
2023-09-03 11:26:32 +0200

    define foutes into main.js

--- src/main.js
+++ src/main.js
@@ -5,4 +5,17 @@ import Home from './components/home.vue'
 import About from './components/about.vue'
 import Movies from './components/movies.vue'
 
-createApp(App).mount('#app')
+const routes = [
+    {path: '/', component: Home},
+    {path: 'about', component: About},
+    {path: 'movies', component: Movies}
+]
+
+const router = createRouter(
+    {
+        history: createWebHistory(),
+        routes
+    }
+)
+
+createApp(App).use(router).mount('#app')
