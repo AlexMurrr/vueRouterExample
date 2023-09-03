35fa418731d18945d582b56475ae0412f1d1f1ce
Alex Mur <lehamuraschov2014@yandex.ru>
2023-09-03 10:59:18 +0200

    fill companents, add import to main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-//import { createApp } from 'vue'
+import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from './App.vue'
+import Home from './components/home.vue'
+import About from './components/about.vue'
+import Movies from './components/movies.vue'
 
 createApp(App).mount('#app')
