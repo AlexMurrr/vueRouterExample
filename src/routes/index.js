cc4d09056a93a60e32f08decc31a81d44abd50b6
Alex Mur <lehamuraschov2014@yandex.ru>
2023-09-08 18:49:00 +0200

    import wiht help function

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
-import Home from '../components/home.vue';
-import About from '../components/about.vue';
-import Movies from '../components/movies.vue';
+
+const Home = () => import( '../components/home.vue');
+const About = () => import( '../components/about.vue');
+
 
 const routes = [
     {path: '/', component: Home},
     {path: '/about', component: About},
-    {path: '/movies', component: Movies}
+    {path: '/movies', component: () => import( '../components/movies.vue')}
 ];
 
 const router = createRouter({
